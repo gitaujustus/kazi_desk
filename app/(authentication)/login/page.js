add893cdeaaa03b3e2e3404e71bb00b1440c5376
kisanyapoulet <kisanyapoulet@gmail.com>
2024-07-01 16:15:06 +0300

    login

--- app/(authentication)/login/page.js
+++ app/(authentication)/login/page.js
@@ -7,56 +7,53 @@ function Login() {
   return (
     <section className='flex justify-center items-center min-h-screen'>
       <div className='flex mx-4'>
-        <div className='w-1/2 bg-gray-200 rounded-l-lg py-8 px-4'>
+        <div className='w-1/2 bg-[#D9D9D9] rounded-l-lg py-8 px-4'>
             <h1 className='font-bold text-center text-3xl mb-16'>Log In</h1>
-            <div className='my-2'>
-            <input 
-                type="email"
-                placeholder='Email'
-            />
-            <h1>Em</h1>
+            <div className='my-2 flex mb-8'>
+              <input 
+                  type="email"
+                  placeholder='Email'
+                   className='py-2 pl-2 rounded-md w-full'
+              />
+              <h1>Em</h1>
             </div>
-            <div className='my-2'>
-            <input 
-                type="password"
-                placeholder='Password'
-            />
-            <h1>Pw</h1>
-            </div>
-
-            <div>
-            <div>
-                <input type="checkbox" />
-                <p>Stay logged in</p>
+            <div className='my-2  flex text-base '>
+              <input 
+                  type="password"
+                  placeholder='Password'
+                  className='py-2 pl-2 rounded-md w-full'
+              />
+              <h1>Pw</h1>
             </div>
 
-            <a href="#">Forgot your password</a>
+            <div className='flex justify-between my-4'>
+              <div className='flex text-sm font-semibold gap-2'>
+                  <input type="checkbox" />
+                  <p>Stay logged in</p>
+              </div>
+
+              <a href="#" className='text-sm text-[#3D63DD] hover:underline font-semibold'>Forgot your password</a>
             </div>
 
-            <button>Log In</button>
+            <button className='text-white text-semibold text-base bg-[#3D63DD] text-center w-full py-2 rounded-lg' >Log In</button>
 
-            <p>Don&apos;t have an account yet?</p>
-            <Link href="/signup">Sign Up</Link>
-
+            <p className='my-3 text-center text-sm font-semibold'>Don&apos;t have an account yet?</p>
+            <p className='text-center'><Link href="/signup" className='text-sm text-center w-full  text-[#3D63DD] hover:underline font-semibold'>Sign Up</Link></p>
 
         </div>
-
-
-
-
-
-
-        <div className='w-1/2 h-auto bg-gray-400 rounded-r-lg py-8 px-4'>
+        <div className='w-1/2 bg-[#172448] rounded-r-lg'>
             <Image
-            src="/next.svg"
+            src="/assets/4.png"
             alt="Next.js Logo"
-            width={100}
-            height={37}
+            width={2560}
+            height={700}
             priority
-            layout='responsive'
-            
+            // layout='responsive'
+            // objectFit='cover'
+            className='bg-red-100 h-full object-cover '
+
             />
-        </div>
+        </div>  
       </div>
     </section>
   )
